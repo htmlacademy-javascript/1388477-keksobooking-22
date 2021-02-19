f2e90c3a67280db8092ca9899bdd85b3c60b3dfc
agro196 <agro_06@mail.ru>
2021-02-19 17:05:15 +0300

    модуль 5 задание 2 финал

--- js/ad-form.js
+++ js/ad-form.js
@@ -1,26 +1,22 @@
 const adForm = document.querySelector('.ad-form')
 const housingType = adForm.querySelector('#type')
 const pricePerNight = adForm.querySelector('#price')
-const checkInTime = adForm.querySelector('#timein')
-const checkOutTime = adForm.querySelector('#timeout')
 const typeToMinPriceRange = {
   flat: 1000,
   bungalow: 0,
   house: 5000,
   palace: 10000,
 }
-housingType.addEventListener('change', () => {
-  pricePerNight.setAttribute('min',typeToMinPriceRange[housingType.value] )
+const initPricePerNight = () =>{
+  pricePerNight.setAttribute('min', typeToMinPriceRange[housingType.value])
   pricePerNight.placeholder = typeToMinPriceRange[housingType.value]
-})
+}
+housingType.addEventListener('change', initPricePerNight)
 
 adForm.addEventListener('change', (evt) => {
-  adForm.timein.value = evt.target.value
-  adForm.timeout.value = evt.target.value
-  console.dir(adForm)
-  console.log(evt.target)
-  console.log(adForm.timein.value)
+  if (evt.target.name === 'timein' || evt.target.name === 'timeout') {
+    adForm.timein.value = evt.target.value
+    adForm.timeout.value = evt.target.value
+  }
 })
-console.log(adForm.timein)
-
-
+initPricePerNight()
